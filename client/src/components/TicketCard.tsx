91b70ff77a53da409d663daefd63158221563324
afeefakvt <afeefakvt6@gmail.com>
2025-07-11 09:15:50 +0530

    final

--- client/src/components/TicketCard.tsx
+++ client/src/components/TicketCard.tsx
@@ -1,4 +1,3 @@
-// components/TicketCard.jsx
 import { FaCheck } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
@@ -19,20 +18,18 @@ const TicketCard = ({
   details,
   price,
   bgColor,
-  textColor,
   tagColor,
   tagTextColor,
 }: TicketCardProps) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div
-        className={`relative rounded-xl p-4 shadow-lg w-full sm:w-[300px] h-full ${bgColor} text-white 
-         ${tag ? 'pt-8' : ''} 
+      className={`relative rounded-xl p-4 shadow-lg w-full sm:w-[300px] h-full ${bgColor} text-white 
+         ${tag ? "pt-8" : ""} 
     before:content-[''] before:absolute before:top-1/2 before:-left-2 before:-translate-y-1/2 
     before:w-6 before:h-6 before:bg-white before:rounded-full
     after:content-[''] after:absolute after:top-1/2 after:-right-2 after:-translate-y-1/2 
     after:w-6 after:h-6 after:bg-white after:rounded-full`}
-      
     >
       {tag && (
         <div
@@ -56,7 +53,10 @@ const TicketCard = ({
 
       <div className="mt-auto flex items-center justify-between">
         <span className="text-sm font-semibold">{price}</span>
-        <button className="bg-white text-black px-3 py-1 rounded text-xs font-medium hover:bg-gray-200 transition" onClick={()=>navigate('/register')}>
+        <button
+          className="bg-white text-black px-3 py-1 rounded text-xs font-medium hover:bg-gray-200 transition"
+          onClick={() => navigate("/register")}
+        >
           Buy Now
         </button>
       </div>
@@ -65,4 +65,3 @@ const TicketCard = ({
 };
 
 export default TicketCard;
-
